10557162c54f6b96b5a8fbd4572ecbfe8c50198f
Lia <liacicati@gmail.com>
2021-03-10 17:31:59 +0100

    feat : connected everything to app

---
+++ app.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-unused-vars */
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const cors = require('cors');
+const { errors } = require('celebrate');
+
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const routes = require('./routes/index');
+const errorHandler = require('./middlewares/errorHandler');
+const NotFoundError = require('./errors/NotFoundError');
+
+const app = express();
+
+const { PORT = 3000 } = process.env;
+
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.use(cors());
+
+app.use(express.json());
+app.use(requestLogger);
+
+app.use(routes);
+
+app.use(errorLogger);
+app.use(errors());
+app.use('*', (req, res) => {
+  throw new NotFoundError('Запрашиваемый ресурс не найден');
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
